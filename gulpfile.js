89f54d3310d607fc787fa826c8a872799e522884
yurii-pavlov <mailyurik@gmail.com>
2019-07-17 20:09:10 +0300

    thene -> plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp');
 var stringReplace = require('gulp-string-replace');
 var config = {
-	theme: {
-        name: 'Starter Kit Theme',	// Change theme name
-		prefix: 'StarterKit',		// Change theme prefix
-		singleton: 'Starter_Kit',	// Change theme main class
+	plugin: {
+        name: 'Starter Kit Plugin',	// Change plugin name
+		prefix: 'StarterKit',		// Change plugin prefix
+		singleton: 'Starter_Kit',	// Change plugin main class
 		textdomain: 'starter-kit',	// Change textdomain
-		styles: 'starter-kit',		// Change CSS styles refix
+		styles: 'starter-kit',		// Change CSS styles prefix
 		author: 'SolidBunch',		// Change author
-		theme_uri: 'https://github.com/SolidBunch/Starter-Kit', 	// Change theme URI
+		plugin_uri: 'https://github.com/SolidBunch/Starter-Kit', 	// Change plugin URI
 		author_uri: 'https://solidbunch.com',	// Change author URI
 	}
 };
@@ -24,31 +24,31 @@ var paths = {
 gulp.task('replaceNames', function () {
 
 	return gulp.src(paths.toReplace.src)
-		.pipe(stringReplace('StarterKit', config.theme.prefix))
-		.pipe(stringReplace('Starter_Kit', config.theme.singleton))
+		.pipe(stringReplace('StarterKit', config.plugin.prefix))
+		.pipe(stringReplace('Starter_Kit', config.plugin.singleton))
 
 		// Styles, classes, ID
-		.pipe(stringReplace('class="starter-kit-', 'class = "' + config.theme.styles + '-'))
-		.pipe(stringReplace('class = "starter-kit-', 'class = "' + config.theme.styles + '-'))
-		.pipe(stringReplace('class="starter-kit_', 'class = "' + config.theme.styles + '_'))
-		.pipe(stringReplace('class = "starter-kit_', 'class = "' + config.theme.styles + '_'))
-		.pipe(stringReplace('#starter-kit-', '#' + config.theme.styles + '-'))
-		.pipe(stringReplace('id="starter-kit-', 'id = "' + config.theme.styles + '-'))
-		.pipe(stringReplace('id = "starter-kit-', 'id = "' + config.theme.styles + '-'))
-		.pipe(stringReplace('\'starter-kit_', '\'' + config.theme.styles + '_'))
-		.pipe(stringReplace('\\.starter-kit-', '.' + config.theme.styles + '-'))
-		.pipe(stringReplace('\\.starter-kit_', '.' + config.theme.styles + '_'))
+		.pipe(stringReplace('class="starter-kit-', 'class = "' + config.plugin.styles + '-'))
+		.pipe(stringReplace('class = "starter-kit-', 'class = "' + config.plugin.styles + '-'))
+		.pipe(stringReplace('class="starter-kit_', 'class = "' + config.plugin.styles + '_'))
+		.pipe(stringReplace('class = "starter-kit_', 'class = "' + config.plugin.styles + '_'))
+		.pipe(stringReplace('#starter-kit-', '#' + config.plugin.styles + '-'))
+		.pipe(stringReplace('id="starter-kit-', 'id = "' + config.plugin.styles + '-'))
+		.pipe(stringReplace('id = "starter-kit-', 'id = "' + config.plugin.styles + '-'))
+		.pipe(stringReplace('\'starter-kit_', '\'' + config.plugin.styles + '_'))
+		.pipe(stringReplace('\\.starter-kit-', '.' + config.plugin.styles + '-'))
+		.pipe(stringReplace('\\.starter-kit_', '.' + config.plugin.styles + '_'))
 
 		// Textdomain
-		.pipe(stringReplace('Text Domain: starter-kit', 'Text Domain: ' + config.theme.textdomain))
-		.pipe(stringReplace('\'starter-kit', '\'' + config.theme.textdomain))
-		.pipe(stringReplace('\"starter-kit', '"' + config.theme.textdomain))
+		.pipe(stringReplace('Text Domain: starter-kit', 'Text Domain: ' + config.plugin.textdomain))
+		.pipe(stringReplace('\'starter-kit', '\'' + config.plugin.textdomain))
+		.pipe(stringReplace('\"starter-kit', '"' + config.plugin.textdomain))
 
-		.pipe(stringReplace('Starter Kit Theme', config.theme.name))
+		.pipe(stringReplace('Starter Kit Plugin', config.plugin.name))
 
-		.pipe(stringReplace('SolidBunch', config.theme.author))
-		.pipe(stringReplace('https://github.com/SolidBunch/Starter-Kit', config.theme.theme_uri))
-		.pipe(stringReplace('https://solidbunch.com', config.theme.author_uri))
+		.pipe(stringReplace('SolidBunch', config.plugin.author))
+		.pipe(stringReplace('https://github.com/SolidBunch/Starter-Kit', config.plugin.plugin_uri))
+		.pipe(stringReplace('https://solidbunch.com', config.plugin.author_uri))
 
 		.pipe(gulp.dest(function (file) {
 			return file.base;  // THE SAME DIR
